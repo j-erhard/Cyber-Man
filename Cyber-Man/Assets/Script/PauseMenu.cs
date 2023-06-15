b6d747218a515202d638a63cb9abe5c7a73311ea
Nicolas <niicolaas67@gmail.com>
2023-06-15 18:48:42 +0200

    f

---
+++ Cyber-Man/Assets/Script/PauseMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        GamePaused = false;
+    }
+
+     void Pause()
+     {
+        pauseMenuUI.SetActive(true);
+        GamePaused = true;
+     }
+}
